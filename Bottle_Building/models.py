6a94eb43123040f3f271d0f60899379a88fc641a
ajn008 <ajn008@bucknell.edu>
2017-08-10 23:08:16 -0400

    another attempt to push to heroku

--- Bottle_Building/models.py
+++ Bottle_Building/models.py
@@ -3,14 +3,30 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Bottle_Building(models.Model):
+    # created by user
+    # bottle walls
+    # foundation
+    # average bottle
     pass
 
 class Bottle(models.Model):
+    # height & units
+    # diameter & units
+    # volume & units
+    # fill mass & units
+    # fill material & density
+
     pass
 
 class Bottle_Wall(models.Model):
+    # mass of fill & units
+    # mass of cement & units
     pass
 
 
 class Foundation(models.Model):
+    # area w/ units
+    # depth w/ units
+    # cement mass w/ units
+
     pass
