6f329237a17de3ca6ff76f0f41d653165a874687
ajn008 <ajn008@bucknell.edu>
2017-12-26 00:15:26 -0500

    admin interface done, not working for dev server but is working for production server ... ?

--- Bottle_Building/admin.py
+++ Bottle_Building/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from Bottle_Building.models import Bottle_Building
+from Bottle_Building.models import Coordinates
 
 # Register your models here.
+admin.site.register(Bottle_Building)
+admin.site.register(Coordinates)
